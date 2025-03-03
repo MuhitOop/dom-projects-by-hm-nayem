1b468da9f5f855444aa112cbfeeceaf9a398bca7
Al Muhit <muhit.al.2002@hotmail.com>
2025-03-03 23:25:40 +0600

    change backgroundcolor by clicking btn done

--- project-01/index.js
+++ project-01/index.js
@@ -1,23 +1,21 @@
 window.onload = () => {
-    main();
-}
+  main();
+};
 
 function main() {
-    const root = document.getElementById('root')
-    const btn = document.getElementById('change-btn');
+  const root = document.getElementById("root");
+  const changeBtn = document.getElementById("change-btn");
 
-    btn.addEventListener('click', function () {
-        const bgcolor = generateRGBColor()
-        root.style.backgroundColor = bgcolor;
-    })
+    changeBtn.addEventListener("click", function () {
+        const bgColor = genarateRGBColor();
+        root.style.backgroundColor = bgColor;
+  });
 }
 
-//randon color genarator function
+function genarateRGBColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
 
-function generateRGBColor() {
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
-
-    return `rgb(${red}, ${green}, ${blue})`;
+  return `rgb(${red}, ${green}, ${blue})`;
 }
