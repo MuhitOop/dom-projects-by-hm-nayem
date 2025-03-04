7a12375b043892a35cff71d8962760becc13f9de
Al Muhit <muhit.al.2002@hotmail.com>
2025-03-04 23:26:51 +0600

    implement the toast msg of the copy hex code

--- project-3/app.js
+++ project-3/app.js
@@ -1,3 +1,6 @@
+// global
+let div = null;
+
 window.onload = () => {
   main();
 };
@@ -5,18 +8,24 @@ window.onload = () => {
 function main() {
   const root = document.getElementById("root");
   const changeBtn = document.getElementById("change-btn");
-    const output = document.getElementById("output");
-    const copyBtn = document.getElementById('copy-btn')
+  const output = document.getElementById("output");
+  const copyBtn = document.getElementById("copy-btn");
 
   changeBtn.addEventListener("click", function () {
     const bgColor = genarateHexColor();
     root.style.backgroundColor = bgColor;
     output.value = bgColor;
   });
-    
-    copyBtn.addEventListener('click', function () {
-        navigator.clipboard.writeText(output.value);
-    })
+
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(output.value);
+
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+    genarateToastMessage(`${output.value} copied`);
+  });
 }
 
 function genarateHexColor() {
@@ -25,4 +34,22 @@ function genarateHexColor() {
   const blue = Math.floor(Math.random() * 255);
 
   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+function genarateToastMessage(msg) {
+  div = document.createElement("div");
+  div.innerText = msg;
+  div.className = "toast-msg toast-msg-slide-in";
+
+  div.addEventListener("click", function () {
+    div.classList.remove("toast-msg-slide-in");
+    div.classList.add("toast-msg-slide-out");
+
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
+    });
+  });
+
+  document.body.appendChild(div);
 }
